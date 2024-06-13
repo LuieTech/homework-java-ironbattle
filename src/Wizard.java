b2957e70924dad8cc42524e93f5df99e55eeb2f2
Gabriel Pena <gabriel@example.com>
2024-06-13 15:47:44 -0400

    Created Wizard Character

--- src/Wizard.java
+++ src/Wizard.java
@@ -1,4 +1,28 @@
 package src;
 
-public class Wizard {
+public class Wizard extends Character{
+        private int mana = (int)(Math.random() * 41) + 10;  // 10 to 50
+        private int intelligence = (int)(Math.random() * 50) + 1;  // 1 to 50
+
+    public Wizard(String name, int hp) {
+        super(name, hp);
+        int randomNum = (int)(Math.random() * 50) + 1;  // 1 to 50
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+    // A public function that overloads attack() implemented in the Attacker
 }
