af363d5bf2a539d206c73e2c8063b28e2c0a76fe
Gabriel Pena <gabriel@example.com>
2024-06-17 12:21:25 -0400

    Added attack methods

--- src/Wizard.java
+++ src/Wizard.java
@@ -1,11 +1,18 @@
 package src;
+import java.util.Random;
 
-public class Wizard extends Character{
-        private int mana = (int)(Math.random() * 41) + 10;  // 10 to 50
-        private int intelligence = (int)(Math.random() * 50) + 1;  // 1 to 50
+public class Wizard extends Character implements Attacker{
+        private int mana;
+        private int intelligence;
+        private Random random;
+        private int damageDealtByWiz;
 
     public Wizard(String name, int hp) {
         super(name, hp);
+        this.mana = (int)(Math.random() * 41) + 10;  // 10 to 50;
+        this.intelligence = (int)(Math.random() * 50) + 1;  // 1 to 50
+        this.random = new Random();
+
     }
 
     public int getMana() {
@@ -23,5 +30,43 @@ public class Wizard extends Character{
     public void setIntelligence(int intelligence) {
         this.intelligence = intelligence;
     }
-    // A public function that overloads attack() implemented in the Attacker
+
+    public void castFireball() {
+        System.out.println("Cast Fireball");
+        mana -= 5;
+    }
+
+    public void staffHit() {
+        System.out.println("Staff hit");
+        mana += 1;
+    }
+
+    public void noManaToAttack() {
+        System.out.println("No mana left to attack");
+        mana += 2;
+    }
+
+    public void randomWizardAttack() {
+        if (mana >= 5) {
+            if (random.nextBoolean()) {
+                castFireball();
+            } else {
+                staffHit();
+            } else if (mana >= 1) {
+                staffHit();
+            } else {
+                noManaToAttack();
+        }
+    }
+
+    @Override
+    public void attack(Character character) {
+        randomWizardAttack();
+    }
+
+
+
+
+
+
 }
