922affe8e315090c11e4e043ee17223e44bb3ca4
elpezpr <gabrielpena@hotmail.com>
2024-06-17 20:10:06 -0400

    fixed main

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,8 @@
-package src;
-
 public class Main {
     public static void main(String[] args) {
-        Wizard gandalf = new Wizard("Gandalf");
-        Wizard sauron =  new Wizard("Sauron");
-
-        System.out.println("Create Wizards and print stats");
-
-        System.out.println("Gandalf HP: " + gandalf.getHp());
-        System.out.println("Gandalf Mana: " + gandalf.getMana());
-        System.out.println("Gandalf Intelligence: " + gandalf.getIntelligence());
-
-        System.out.println("Sauron HP: " + sauron.getHp());
-        System.out.println("Sauron Mana: " + sauron.getMana());
-        System.out.println("Sauron Intelligence: " + sauron.getIntelligence());
-
-        System.out.println("Battle");
-
-        while (gandalf.isAlive() && sauron.isAlive()) {
-            gandalf.attack(sauron);
-            sauron.attack(gandalf);
-        }
-        if (gandalf.isAlive()) {
-            System.out.println("Gandalf wins!");
-        } else {
-            System.out.println("Sauron wins!");
-        }
-
-        System.out.println("Check Stats");
-
-        System.out.println("Gandalf HP: " + gandalf.getHp());
-        System.out.println("Gandalf Mana: " + gandalf.getMana());
-        System.out.println("Gandalf Intelligence: " + gandalf.getIntelligence());
-
-        System.out.println("Sauron HP: " + sauron.getHp());
-        System.out.println("Sauron Mana: " + gandalf.getMana());
-        System.out.println("Sauron Intelligence: " + gandalf.getIntelligence());
+        Character test = new Character("Dan", 500);
+        System.out.println(test.getId());
 
 
     }
 }
-
