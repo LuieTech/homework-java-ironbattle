bedb56a35822200bc6e704e8f623eecd5462ca18
LuieR <rhluisenrique@gmail.com>
2024-06-17 21:17:29 -0400

    chore: Warrior class functionality complete

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,6 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-//        Wizard gandalf = new Wizard("Gandalf");
-//        Wizard sauron =  new Wizard("Sauron");
-//
-//        System.out.println("Create Wizards and print stats");
-//
-//        System.out.println("Gandalf HP: " + gandalf.getHp());
-//        System.out.println("Gandalf Mana: " + gandalf.getMana());
-//        System.out.println("Gandalf Intelligence: " + gandalf.getIntelligence());
-//
-//        System.out.println("Sauron HP: " + sauron.getHp());
-//        System.out.println("Sauron Mana: " + sauron.getMana());
-//        System.out.println("Sauron Intelligence: " + sauron.getIntelligence());
-//
-//        System.out.println("Battle");
-//
-//        while (gandalf.isAlive() && sauron.isAlive()) {
-//            gandalf.attack(sauron);
-//            sauron.attack(gandalf);
-//        }
-//        if (gandalf.isAlive()) {
-//            System.out.println("Gandalf wins!");
-//        } else {
-//            System.out.println("Sauron wins!");
-//        }
-//
-//        System.out.println("Check Stats");
-//
-//        System.out.println("Gandalf HP: " + gandalf.getHp());
-//        System.out.println("Gandalf Mana: " + gandalf.getMana());
-//        System.out.println("Gandalf Intelligence: " + gandalf.getIntelligence());
-//
-//        System.out.println("Sauron HP: " + sauron.getHp());
-//        System.out.println("Sauron Mana: " + gandalf.getMana());
-//        System.out.println("Sauron Intelligence: " + gandalf.getIntelligence());
-
-        //       Warrior Testing
-//        Warrior aragorn = new Warrior("Aragorn", 300, 30, 7);
-//        Warrior legolas = new Warrior("Legolas", 215, 110, 15);
-//
-//        System.out.println("Aragon HP: " + aragorn.getHp());
-//        System.out.println("Aragorn object: " + aragorn.toString());
-//
-//        System.out.println("Legolas strenght: " + legolas.getStrength());
-//        System.out.println("Legolas: " + legolas.toString());
 
 
     }
