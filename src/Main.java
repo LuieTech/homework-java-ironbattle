d0a91edfd15e09cbcc6efd696a219dbf8e006d47
LuieR <rhluisenrique@gmail.com>
2024-06-17 21:31:32 -0400

    cleaning the Main comments tests

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
+package src;
+
 public class Main {
     public static void main(String[] args) {
-        Character test = new Character("Dan", 500);
-        System.out.println(test.getId());
-
 
     }
 }
